37028f9c41accad58141f5aba3fcd53fa8bbaca8
asahel <asahel.c@gmail.com>
2021-04-30 17:28:52 +0300

    set problem+hpp work

--- Dispatcher.hpp
+++ Dispatcher.hpp
@@ -5,9 +5,6 @@ namespace pandemic
     //can fly from city with research center to anywhere without using city card
     class Dispatcher // : public Player
     {
-    // public:
-    //     Dispatcher(Board, City);
-    //     std::string role();
-    //     Dispatcher &fly_direct(City);
+
     };
 }
